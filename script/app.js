fd31f2702e30f56e3c293950976816f5b431fa7d
fzed51 <fzed51@yahoo.fr>
2014-05-22 14:16:22 +0200

    Update
    Progression dans la construction du popup et du style.
    Mise en place de fonction pour la manipulation su DOM

--- script/app.js
+++ script/app.js
@@ -1,7 +1,29 @@
+var Q = function(querry){return document.querySelector(querry);};
+var Qs = function(querry){return document.querySelectorAll(querry);};
+var SE = function(item){return item.parentNode.removeChild(item);}
+var NE = function(tag, id, clas){
+	var item = document.createElement(tag);
+	if(id){item.setAttribute('id',id);}
+	if(clas){item.className=clas;}
+	return item;
+}
+var Ap = function(Parent, Child){Parent.appendChild(Child);}
+var Pr = function(Parent, Child){Parent.insertBefore(Child,Parent.firstChild);}
+var St = function(item, styles){
+	var camelCase = function(string){
+			return string.replace(/^-ms-/, "ms-" ).replace(/-([\da-z])/gi, function(all, letter){
+					return letter.toUpperCase();
+				});
+		};
+		for(prop in styles){
+			try{item.style[camelCase(prop)]=styles[prop];}catch(e){}
+		}		
+}
+
 var wait = function(fnEnd, refrech, msg){
 	if(!fnEnd()){
 		affiche('wait', msg)
-		setTimeout('wait', refrech, fnEnd, refrech, msg);
+		setTimeout(wait, refrech, fnEnd, refrech, msg);
 	} else {
 		cache('wait')		
 	}
@@ -11,42 +33,100 @@ var progress = function(fnProgress, refrech, msg, max){
 	var prog = fnProgress()
 	if(prog < max){
 		affiche('progress', msg, prog, max)
-		setTimeout('progress', refrech, fnProgress, refrech, msg, max);
+		setTimeout(progress, refrech, fnProgress, refrech, msg, max);
 	} else {
-		cache('wait')		
+		cache('progress')		
 	}
 };
 
 var affiche = function(type, msg, progress, max){
 	var doc = document,
 		body = doc.body,
-		Q = function(querry){return doc.querySelector(querry);},
 		elem;
 	if(!Q('#Widget'+type)){
 		elem = creerElement(type);
 	} else {
 		elem = Q('#Widget'+type)
 	}
+	oMsg = Q('#Widget'+type+' .message')
+	oMsg.innerText = msg;
 };
 
 var cache = function(type){
-	var Q = function(querry){return document.querySelector(querry);},
-		elem;
 	if(Q('#Widget'+type)){
 		setTimeout('supprimeElement', 4000, type);
 	}
 };
 
 var creerElement = function(type){
-	
+	var doc = document,
+		body = doc.body,
+		gfi = NE('div','Widget'+type,'gfi'),
+		popup = NE('div',null,'popup'),
+		msg = NE ('p',null,'message');
+	St(gfi, {
+		'position':'absolute',
+		'width':'100%',
+		'height':'100%',
+		'top':0,
+		'background-color':'#888',
+		'background-color':'rgba(0,0,0,0.5)',
+		'z-Index':999999999
+	});
+	St(popup, {
+		'position':'absolute',
+		'left':'50%',
+		'top':'50%',
+		'width':'300px',
+		'margin-left':'-150px',
+		'background-color':'#bbf',
+		'border':'solid #028 2px',
+		'border-radius':'7px',
+		'padding':'7px'
+	})
+	St(msg, {
+		'color':'#eee',
+		'text-shadow':'1px 1px 3px #000',
+		'font-family':'sans-serif',
+		'font-size':'20px'
+	})
+	Ap(popup, msg);
 	switch(type){
-		case 'wait':
+		case 'wait':			
 			break;
 		case 'progress':
+				var over = NE('div',null,'overlay'),
+					prog = NE('div',null, 'bar');
+				Ap(popup, prog);
+				Ap(popup, over);
+				St(over,{
+					'position':'relative',
+					'background-color':'rgba(1,1,1,0.1)',
+					'-webkit-box-shadow':'inset 2px 2px 4px 0 #FFFFFF',
+					'box-shadow':'inset 2px 2px 4px 0 #FFFFFF',
+					'margin-left':'10px',
+					'margin-right':'10px',
+					'height':'20px',
+					'border-radius':'10px'					
+				})
+				St(prog,{
+					'position':'absolute',
+					'background-color':'#0f0',
+					'-webkit-box-shadow':'inset 2px 2px 4px 0 #FFFFFF',
+					'box-shadow':'inset 2px 2px 4px 0 #FFFFFF',
+					'margin-top':'4px',
+					'margin-left':'15px',
+					'margin-right':'15px',
+					'width':'10px',
+					'height':'12px',
+					'border-radius':'5px'				
+				})
 			break;
 	}
+	Ap(gfi, popup);
+	Ap(body, gfi);
 }
 
 var supprimeElement = function(type){
-
+	SE(Q('#Widget'+type));
 }
